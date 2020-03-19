f8679d461d9ba92384341ec4bba6cb335ab7857c
Sean Callaway <seancallaway@gmail.com>
2020-03-19 16:19:04 -0700

    Added task models and tests.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,37 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    name = models.CharField(max_length=75)
+    last_start = models.DateTimeField(null=True, default=None)
+    last_finish = models.DateTimeField(null=True, default=None)
+    avg_duration = models.DurationField(null=True, default=None)
+
+    def __str__(self):
+        return self.name
+
+    def calc_avg_duration(self):
+        """
+        Calculates the average duration based on TaskHistory.
+
+        Returns average duration as a timedelta or None.
+        """
+        history_length = self.history.count()
+        if history_length > 0:
+            durations = self.history.values_list('duration', flat=True)
+            avg = sum(durations, datetime.timedelta(0)) / history_length
+            return avg
+        return None
+
+
+class TaskHistory(models.Model):
+
+    task = models.ForeignKey(Task, related_name='history', on_delete=models.CASCADE)
+    end_time = models.DateTimeField()
+    duration = models.DurationField()
+
+    def save(self, *args, **kwargs):
+        super(TaskHistory, self).save(*args, **kwargs)
+        self.task.avg_duration = self.task.calc_avg_duration()
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timedelta
 from django.test import TestCase
 from django.urls import resolve
+from tasks.models import Task
 from tasks.views import HomePage
 
 BOOTSTRAP_VERSION = 'bootstrap/4.4.1'
@@ -14,3 +16,19 @@ class HomePageTest(TestCase):
     def test_home_page_includes_bootstrap(self):
         response = self.client.get('/')
         self.assertContains(response, BOOTSTRAP_VERSION)
+
+
+class TaskTest(TestCase):
+
+    def setUp(self):
+        self.task = Task.objects.create(name='Test Task')
+        self.task.history.create(end_time=datetime(2020, 3, 19, 15, 7, 0), duration=timedelta(minutes=7))
+        self.task.history.create(end_time=datetime(2020, 3, 19, 14, 6, 0), duration=timedelta(minutes=6))
+        self.task.history.create(end_time=datetime(2020, 3, 19, 13, 5, 0), duration=timedelta(minutes=5))
+
+    def test_avg_calculation(self):
+        result = self.task.calc_avg_duration()
+        self.assertEqual(result, timedelta(minutes=6))
+
+    def test_saved_avg_calculation(self):
+        self.assertEqual(self.task.avg_duration, timedelta(minutes=6))
