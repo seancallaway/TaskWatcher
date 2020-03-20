6b936e85570a31b78a344299e7a0e1f96ba843a2
Sean Callaway <seancallaway@gmail.com>
2020-03-20 08:28:33 -0700

    Added task table to home page.

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from tasks.models import Task
 
 
-class HomePage(TemplateView):
+class HomePage(ListView):
     template_name = 'index.html'
-
+    model = Task
+    queryset = Task.objects.all().order_by('last_start')
+    context_object_name = 'tasks'
